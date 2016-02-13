e3545594b850dd9be2717d8f052094c71c492676
tianyu1991 <hulan1991@gmail.com>
2016-02-12 21:40:34 -0500

    Update taxi.r

--- taxi.r
+++ taxi.r
@@ -23,7 +23,10 @@ library(dplyr)
 data2<-group_by(data,pickup_hour)
 data3<-summarise(data2,count=n(),mean_longitude=mean(pickup_longitude),mean_latitude=mean(pickup_latitude))
 library(ggplot2)
-ggplot(data3,aes(mean_longitude,mean_latitude))+geom_point(aes(size=count,colour=factor(pickup_hour)),alpha=1/2)+scale_size_area()+ggtitle("NYC taxi pick up location")
+
+png(file="PickuplocationEachHour",width=780,height=480)
+ggplot(data3,aes(mean_longitude,mean_latitude))+geom_point(aes(size=count,colour=factor(pickup_hour)),alpha=1/2)+scale_size_area()+ggtitle("NYC taxi pick up location")+xlab("mean longitude")+ylab("mean latitude")
+dev.off()
 
 char_i="00"
 pickup_hour2<-as.numeric(data$pickup_hour)-1
@@ -34,22 +37,40 @@ for(i in 0:23){
 	sub_data<-data[pickup_hour2==i,]
 	s2<-group_by(sub_data,pickup_location)
 	s3<-		summarise(s2,count=n(),mean_longitude=mean(pickup_longitude),mean_latitude=mean(pickup_latitude))
-	location2[i+1,c(2,3,4)]<-arrange(s3,desc(count))[c(2),c(2,3,4)]
+	location2[i+1,c(2,3,4)]<-arrange(s3,desc(count))[c(1),c(2,3,4)]
 }
-ggplot(location2,aes(mean_longitude,mean_latitude))+geom_point(aes(size=count,colour=factor(hour)),alpha=1/4)+scale_size_area()+ggtitle("NYC taxi pick up location2")
+
+png(file="MostPickuplocationEachHour",width=780,height=480)
+ggplot(location2,aes(mean_longitude,mean_latitude))+geom_point(aes(size=count,colour=factor(hour)),alpha=1/4)+scale_size_area()+ggtitle("most frequent taxi pick up location")+xlab("mean longitude")+ylab("mean latitude")
+dev.off()
+
 
 sub_data<-data[pickup_hour2==18,]
 velocity<-sub_data$trip_distance/sub_data$trip_time_in_secs*360
 velocity[is.na(velocity)|is.infinite(velocity)]<-0
 velocity[velocity>quantile(velocity, 0.999)]<-0
 velocity[velocity==0]<-NA
+mean<-mean(velocity[!is.na(velocity)])
 sub_data$velocity<-velocity
+velocity[!is.na(velocity)]<-velocity[!is.na(velocity)]/mean
+sub_data<-cbind(sub_data,velocity_index=velocity)
+distance_index<-sub_data$trip_distance/mean(sub_data$trip_distance)
+sub_data<-cbind(sub_data,distance_index)
+sub_data2<-sub_data[!is.na(sub_data$velocity),]
 
-sub_data<-cbind(sub_data,velocity)
+## set de index for velocity and trip_distance
+##assume the index for velocity is 0.4
+##assume the index for trip distance is 0.3
+##assume 2,1 
+##assume 
+i_vel<-2
+i_dis<-1
 
+index<-sub_data2$velocity_index^i_vel*sub_data2$distance_index^i_dis
+sub_data2<-cbind(sub_data2,index)
 
-s2<-group_by(sub_data,pickup_location)
-s3<-		summarise(s2,count=n(),mean_longitude=mean(pickup_longitude),mean_latitude=mean(pickup_latitude),mean_velocity=mean(velocity,na.rm=TRUE))
+s2<-group_by(sub_data2,pickup_location)
+s3<-		summarise(s2,score=sum(index),count=n(),mean_longitude=mean(pickup_longitude),mean_latitude=mean(pickup_latitude),mean_velocity=mean(velocity,na.rm=TRUE))
 
 lo_q<-quantile(s3$mean_longitude, c(.05, .95)) 	
 la_q<-quantile(s3$mean_latitude, c(.05, .95)) 	
@@ -57,7 +78,14 @@ la_q<-quantile(s3$mean_latitude, c(.05, .95)) 
 s4<-
 s3[(s3$mean_longitude>lo_q[1])&(s3$mean_longitude<lo_q[2])&(s3$mean_latitude>la_q[1])&(s3$mean_latitude<la_q[2]),]
 
-ggplot(s4,aes(mean_longitude,mean_latitude))+geom_point(aes(colour=count),size=0.2,alpha=1/3)+scale_size_area()+ggtitle("NYC taxi pick up nums at 6pm ")+xlab("longitude")+ylab("latitude")
+png(file="bestpickupcount",width=780,height=480)
+ggplot(s4,aes(mean_longitude,mean_latitude))+geom_point(aes(colour=count),size=0.2,alpha=1/3)+scale_size_area()+ggtitle("pick up counts at 6pm ")+xlab("longitude")+ylab("latitude")
+dev.off()
 
-ggplot(s4,aes(mean_longitude,mean_latitude))+geom_point(aes(colour=mean_velocity),size=0.2,alpha=1/3)+scale_size_area()+ggtitle("average speed of trip with different pick up location at 6pm ")+xlab("longitude")+ylab("latitude")
+png(file="averagespeed",width=780,height=480)
+ggplot(s4,aes(mean_longitude,mean_latitude))+geom_point(aes(colour=mean_velocity),size=0.2,alpha=1/3)+scale_size_area()+ggtitle("average speed of trip from different pick up location at 6pm ")+xlab("longitude")+ylab("latitude")
+dev.off()
 
+png(file="bestpickup5002",width=780,height=480)
+ggplot(s4,aes(mean_longitude,mean_latitude))+geom_point(aes(colour=score),size=0.2,alpha=1/3)+scale_size_area()+ggtitle("best pick up location at 6pm (score range:0:500)")+xlab("longitude")+ylab("latitude")+scale_colour_gradient(limits=c(0, 500))
+dev.off()
